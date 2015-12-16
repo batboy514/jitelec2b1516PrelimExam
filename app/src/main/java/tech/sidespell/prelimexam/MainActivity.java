a4d8f9f642756ae598d1bd50869ab9ab5e6db89d
jethro <esplanadajethro@gmail.com>
2015-12-15 16:35:56 -0800

    Prelim Exam

--- app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
+++ app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
@@ -1,17 +1,99 @@
 package tech.sidespell.prelimexam;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+        TextView txtSeekbar,txtTime;
+        SeekBar seekbar;
+        ToggleButton toggle;
+        RadioButton r1,r2;
+        int discrete=0;
+        int start=0;
+        int end=1000;
+        int startPosition=0;
+        int endPosition=0;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start=-1;
+        end=-1;
+
+        txtSeekbar=(TextView)findViewById(R.id.textView);
+        txtTime=(TextView)findViewById(R.id.textView2);
+        seekbar=(SeekBar)findViewById(R.id.seekBar);
+        toggle=(ToggleButton)findViewById(R.id.toggleButton);
+        r1=(RadioButton)findViewById(R.id.radioButton);
+        r2=(RadioButton)findViewById(R.id.radioButton2);
+
+
+        seekbar.setProgress(startPosition);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int temp = startPosition;
+                int dis = end - start;
+                discrete = (start + ((temp / 1000) * dis));
+            }
+        });
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                AssetManager assetManager = getAssets();
+                Typeface customFont   = Typeface.createFromAsset(assetManager, "fonts/SourceSansPro_Light.otf");
+                txtTime.setTypeface(customFont);
+
+                final Handler handler = new Handler();
+
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        startPosition -= 1000;
+                        txtTime.setText(startPosition + "");
+
+                        if (startPosition > 0) {
+                            handler.postDelayed(this, 1000);
+
+                        }
+                    }
+                };
+                handler.postDelayed(runnable,1000);
+            }
+
+        });
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
